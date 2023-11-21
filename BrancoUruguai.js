6287ff2261befb9852273c35ee767bf5e1ec8f8a
Marco Soares <139500504+DevMrc@users.noreply.github.com>
2023-11-20 21:18:19 -0300

    Update BrancoUruguai.js

--- BrancoUruguai.js
+++ BrancoUruguai.js
@@ -23,7 +23,7 @@ const BrancoUruguai = ({ navigation }) => {
       <TouchableOpacity onPress={() => navigation.navigate("origem")}>
         <Text style={styles.texto}>Home</Text>
       </TouchableOpacity>
-      <TouchableOpacity onPress={() => navigation.navigate("MinhaTela")}>
+      <TouchableOpacity onPress={() => navigation.navigate("sobre")}>
         <Text style={styles.texto}>Sobre</Text>
       </TouchableOpacity>
       <TouchableOpacity  onPress={() => navigation.navigate("carrinho")}>
